6b07fcf9ce19285af52fa1145aabdd17ecdb081f
Marcel Göhn <marcelgoehn@googlemail.com>
2025-02-16 00:15:28 +0100

    fetched evolution chain

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 let BASE_URL = "https://pokeapi.co/api/v2/pokemon?limit=40&offset=0";
 let pokemons = [];
 let pokemonsData = [];
+let pokemonsFetchedEvoChain = [];
 const dialog = document.getElementById('overlay');
 const wrapper = document.querySelector('.wrapper');
 
@@ -35,7 +36,9 @@ async function fetchPokemons() {
 async function storePokemonData() {
     for (let i = 0; i < pokemons.length; i++) {
         let pokemonInformation = await fetchPokemonData(i);
+        let pokemonEvoChain = await fetchEvoChain(pokemonInformation);
         pokemonsData.push(pokemonInformation);
+        pokemonsFetchedEvoChain.push(pokemonEvoChain);
     }
     renderPokemonCards();
 }
@@ -49,6 +52,26 @@ async function fetchPokemonData(index) {
         }
         let data = await response.json();
         return data;
+    }
+    catch(error) {
+        console.error("Fehler beim Abrufen der Daten:", error);
+    }
+}
+
+
+async function fetchEvoChain(pokemonInformation) {
+    try {
+        let firstResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonInformation.id}`);
+        if(!firstResponse.ok) {
+            throw new Error(`HTTP Fehler! Status: ${firstResponse.status}`);
+        }
+        let firstData = await firstResponse.json();
+        let secondResponse = await fetch(firstData.evolution_chain.url);
+        if(!secondResponse.ok) {
+            throw new Error(`HTTP Fehler! Status: ${secondResponse.status}`);
+        }
+        let secondData = await secondResponse.json();
+        return secondData;
     }
     catch(error) {
         console.error("Fehler beim Abrufen der Daten:", error);
@@ -101,6 +124,7 @@ function showStats(i) {
 
 
 console.log(pokemonsData);
+console.log(pokemonsFetchedEvoChain);
 
 
 
